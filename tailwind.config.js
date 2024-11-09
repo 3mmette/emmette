554d0f094e159da60da86dcc6cae4c3992acc744
Emmette Taylor <emmette.taylor@hotmail.com>
2024-11-09 12:33:45 +0800

    feat: TailwindCSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'about.html',
+    'contact.html',
+    'index.html',
+    'mywork.html',
+    'projects.html',
+    'resume.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
